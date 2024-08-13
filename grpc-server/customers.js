64eb432ea6da53a0c83d2ac47a2ad4973f485280
rajdip14 <palrajdip33@gmail.com>
2024-08-13 21:08:39 +0530

    first commit

---
+++ grpc-server/customers.js
@@ -0,0 +1,28 @@
+const customers = [
+    {
+        id: 'iwer923ece23r',
+        name: 'Rajdip Pal',
+        age: 22,
+        address: 'Kolkata'
+    },
+    {
+        id: 'jsdfy8u93irr9',
+        name: 'Souvik Maji',
+        age: 25,
+        address: 'Kolkata'
+    },
+    {
+        id: 'fsdudsf89uwu',
+        name: 'Sayantan Majumdar',
+        age: 24,
+        address: 'Kolkata'
+    },
+    {
+        id: 'df43feww3423rr',
+        name: 'Sandip Roy',
+        age: 22,
+        address: 'Chinsurah'
+    }
+]
+
+module.exports = customers;
